1103e7f510363e6aa2db0757f7ae58683ec2ee6b
Mykola Dovhopol <97382411+lifesheets@users.noreply.github.com>
2023-02-17 16:08:51 +0200

    [Lib] React 18.2.0 | Створення користувацьких інтерфейсів.

---
+++ interface/src/example/Example.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Example() {
+  return (
+    <div className='wixcore'>
+      <h1>Тестуємо роботу у ReactJS.</h1>
+    </div>
+  );
+}
+
+export default Example;
---
+++ interface/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Example } from './example';
+
+const root = ReactDOM.createRoot(document.getElementById('ragemp-wixcore'));
+
+root.render(
+  <React.StrictMode>
+    <Example />
+  </React.StrictMode>
+);
